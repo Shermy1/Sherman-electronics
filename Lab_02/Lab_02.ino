3dc2003a01d4886f226dea00def2e0ea1acfb177
unknown <MichaelSherman16@MSherman16.MaristSchool.Local>
2014-12-15 13:09:14 -0500

    Added Lab_02

--- Lab_02/Lab_02.ino
+++ Lab_02/Lab_02.ino
@@ -6,6 +6,7 @@
 
 void blink() {
   int pins [] = {2, 3, 4, 5, 6, 7, 8, 9};
+  //quickly runs from pin 2 to pin 9 and turns off the previous light. Goes all the way back 
   for (int i = 0; i <8; i++) {
     digitalWrite(pins[i], HIGH);
     delay(50);
@@ -18,6 +19,7 @@ void blink() {
     digitalWrite(pins[i], LOW);
     delay(50);
   }//End for loop 2
+  //more slowly goes from pin 2 to pin 9 and keeps them on throughout it. Goes all the way back in reverse.
   for (int i = 0; i < 8; i++) {
     digitalWrite(pins[i], HIGH);
     delay(100);
@@ -26,12 +28,14 @@ void blink() {
     digitalWrite(pins[i], LOW);
     delay(100);
   }//End loop 4
+  //Breaks up the LED's turning all the way on and staying on with another first for loop pattern
   for (int i = 0; i <8; i++) {
     digitalWrite(pins[i], HIGH);
     delay(50);
     digitalWrite(pins[i], LOW);
     delay(50);
   }//End loop 5
+  //performs the same for loop as 3 and 4 except backwards.
   for (int i = 8; i > 0; i--) {
     digitalWrite(pins[i], HIGH);
     delay(100);
@@ -40,6 +44,7 @@ void blink() {
     digitalWrite(pins[i], LOW);
     delay(100);
  }// End loop 7
+ //Turns the ends on, pins 2 and 9, and slowly works its way to the center and then crosses.
  for (int i = 0; i < 8; i++) {
    digitalWrite(pins[i], HIGH);
    delay(100);
